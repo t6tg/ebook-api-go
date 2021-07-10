71fa04be4359ca57757c286919126f29790e3b3d
Thanawat Gulati <work.t6tg@gmail.com>
2021-07-11 01:07:16 +0700

    feat: poc encrypt decrypt

---
+++ go.mod
@@ -0,0 +1,9 @@
+module ebook
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.2
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+)
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Upload File",
+		})
+	})
+
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.POST("/upload", func(c *gin.Context) {
+		start := time.Now()
+		file, _ := c.FormFile("file")
+		if err := c.SaveUploadedFile(file, "./uploads/"+file.Filename); err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"message": "Unable to save the file",
+			})
+		}
+		key, _ := hex.DecodeString("554fa21d43a6f294fe34746ff1481ffaef81589d8420c5cf179ba55536ae9a5d")
+		data, err := ioutil.ReadFile("./uploads/" + file.Filename)
+		if err != nil {
+			log.Print("read error")
+			log.Fatal(err)
+		}
+		block, err := aes.NewCipher(key)
+		if err != nil {
+			log.Print("read block")
+			panic(err.Error())
+		}
+		gcm, err := cipher.NewGCM(block)
+		if err != nil {
+			log.Panic(err)
+		}
+		nonce := make([]byte, gcm.NonceSize())
+		if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+			log.Fatal(err)
+		}
+		ciphertext := gcm.Seal(nonce, nonce, data, nil)
+		err = ioutil.WriteFile("./uploads/"+file.Filename, ciphertext, 0777)
+		if err != nil {
+			log.Print("read write")
+			panic(err.Error())
+		}
+		end := time.Now()
+		elapsed := end.Sub(start)
+		c.JSON(http.StatusOK, gin.H{
+			"status": http.StatusOK,
+			"time":   elapsed,
+		})
+	})
+
+	router.GET("/download/:filename", func(c *gin.Context) {
+		start := time.Now()
+		filename := c.Param("filename")
+		key, _ := hex.DecodeString("554fa21d43a6f294fe34746ff1481ffaef81589d8420c5cf179ba55536ae9a5d")
+		ciphertext, err := ioutil.ReadFile("./uploads/" + filename)
+		block, err := aes.NewCipher(key)
+		if err != nil {
+			log.Panic(err)
+		}
+		gcm, err := cipher.NewGCM(block)
+		if err != nil {
+			log.Panic(err)
+		}
+		nonce := ciphertext[:gcm.NonceSize()]
+		ciphertext = ciphertext[gcm.NonceSize():]
+		plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+		if err != nil {
+			log.Panic(err)
+		}
+		err = ioutil.WriteFile("./uploads/"+filename, plaintext, 0777)
+		if err != nil {
+			log.Print("read write")
+			panic(err.Error())
+		}
+		end := time.Now()
+		elapsed := end.Sub(start)
+		c.JSON(http.StatusOK, gin.H{
+			"status": http.StatusOK,
+			"time":   elapsed,
+		})
+	})
+	router.Run(":8080")
+}
